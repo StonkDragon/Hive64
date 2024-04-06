7686c21903cf96353c13c54a71c0cd3f296b0113
TemperΘΔ <theanonym919@gmail.com>
2024-04-06 18:49:21 +0200

    E

--- example.sh
+++ example.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-hive64-unknown-as main.hive64 libc.hive64
-hive64-unknown-ld main.rcx -lc -o main
-hive64-unknown-ld libc.rcx -o libc.dll
+./hive64-unknown-as main.hive64 libc.hive64
+./hive64-unknown-ld main.rcx -lc -o main
+./hive64-unknown-ld libc.rcx -o libc.dll
